9dc9e2d9bcdb91e686f4a335e4f8bf42708237d9
Karolina Redden <dubajkaro@gmail.com>
2021-12-13 17:34:24 +0000

    for loop packing list

--- packing-list.js
+++ packing-list.js
@@ -1,10 +1,9 @@
 
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-
 console.log("Kitchen stuff to pack:");
-let i = 0;
-while (i < packingList.length) {
+for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
-  i++;
 }
+
+packingList[3];
